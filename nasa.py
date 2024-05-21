d539ba8aaf61ec0343a0f6114be096cab86b881a
MatyasP123 <29447070@fsv.cuni.cz>
2024-05-21 20:45:46 +0200

    new file

--- nasa.py
+++ nasa.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 import matplotlib.pyplot as plt
 plt.ion()
 import matplotlib.pyplot as plt
@@ -21,21 +16,8 @@ import boto3
 from IPython.display import HTML, display
 import datetime
 
-
-# In[2]:
-
-
 nasa = requests.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-09-07&end_date=2023-09-08&api_key=XwVHPFh2zHGkoQZ3QmLtDqyncNjhdloY07lr9868")
-
-
-# In[3]:
-
-
 nasa.json()
-
-
-# In[4]:
-
 
 import datetime
 
@@ -58,18 +40,7 @@ def iterate_over_dates(start_date_str, end_date_str):
             start_date += datetime.timedelta(days=1)  # Increment the date by one day
             
         return date_strings
-            
-    except Exception as e:
-        return str(e)
 
-
-# In[ ]:
-
-
-
-
-
-# In[5]:
 
 
 def request_nasa(start_date_str, end_date_str):
@@ -85,13 +56,7 @@ def request_nasa(start_date_str, end_date_str):
         print(e)
 
 
-# In[6]:
-
-
 request_nasa("2023-08-25","2023-08-27")
-
-
-# In[7]:
 
 
 def request_nasa_one_day(date_str):
@@ -107,23 +72,8 @@ def request_nasa_one_day(date_str):
         print(e)
 
 
-# In[8]:
-
 
 days_from_period = iterate_over_dates("2023-08-25","2023-09-27")
-
-
-# In[9]:
-
-
-days_from_period
-
-
-# 
-
-# In[10]:
-
-
 raw = pd.DataFrame()
 for day in days_from_period:
     neo_data = pd.DataFrame((request_nasa_one_day(day)["near_earth_objects"][day]))
@@ -131,52 +81,17 @@ for day in days_from_period:
     df.insert(1, 'date', day)
     raw = pd.concat([raw, df], ignore_index=True)
 
-
-# In[11]:
-
-
-raw
-
-
-# In[12]:
-
-
-raw.columns
-
-
-# In[13]:
-
-
 dia = raw["estimated_diameter"]
 close = raw['close_approach_data']
 close
-
-
-# In[14]:
-
-
 dia_list = dia.tolist()
 dia_units_df = pd.json_normalize(dia_list)
-
-
-# In[15]:
-
-
 col_index_1 = raw.columns.get_loc("estimated_diameter")
 left = raw.iloc[:, :col_index_1]
 right = raw.iloc[:, col_index_1:]
-
-
-# In[16]:
-
-
 result_df = pd.concat([left, dia_units_df, right], axis=1)
 result_df = result_df.drop('estimated_diameter', axis=1)
 result_df
-
-
-# In[17]:
-
 
 close_df = pd.DataFrame([x[0] for x in close])
 close_df
@@ -187,16 +102,8 @@ miss = close_df['miss_distance']
 miss_list = miss.tolist()
 miss_units_df = pd.json_normalize(miss_list)
 
-
-# In[18]:
-
-
 close_df = close_df.drop(['relative_velocity', 'miss_distance'], axis=1)
 close_df
-
-
-# In[19]:
-
 
 col_index_close = result_df.columns.get_loc("close_approach_data")
 result_df = result_df.drop('close_approach_data', axis=1)
@@ -205,35 +112,12 @@ right_close = result_df.iloc[:, col_index_close:]
 final_df= pd.concat([left_close, close_df, vel_units_df, miss_units_df , right_close], axis=1)
 final_df
 
-
-# In[20]:
-
-
 column_names_mapping = {'kilometers_per_second': 'relative_velocity_km/s', 'kilometers_per_hour': 'relative_velocity_km/h','miles_per_hour': 'relative_velocity_m/h',"astronomical": "miss_dist_astromnomical","lunar": "miss_dist_lunar","kilometers": "miss_dist_km","miles": "miss_dist_miles"}
 
-
-# In[21]:
-
-
 final_df = final_df.rename(columns=column_names_mapping)
-
-
-# In[22]:
-
-
 final_df.head().style
 
 
-# In[23]:
-
-
-final_df.columns
-
-
-# In[24]:
-
-
-final_df.to_csv('final_df.csv', index=False)
-
 def get_dataframe():
     return final_df
+
